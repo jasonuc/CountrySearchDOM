db4a77decdae4347a464f5cbaba08fc22bd725ae
jasonuc <mrjasonu@gmail.com>
2023-10-27 17:26:25 +0100

    adds html and styling for the header

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,26 @@ module.exports = {
     "./public/**/*.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        charcoal: "#264653",
+        persianGreen: "#2a9d8f",
+        saffron: "#e9c46a",
+        sandyBrown: "#f4a261",
+        burntSienna: "#e76f51"
+      },
+      fontFamily: {
+        libreCaslonDisplay: ['Libre Caslon Display', 'ui-serif'],
+        libreCaslonText: ['Libre Caslon Text', 'ui-serif'],
+        oswald: ['Oswald', 'ui-sans'],
+      },
+      backgroundImage: {
+        globeBg: "url('../images/globe-2.jpg')",
+        // globeBg: "url('./public/images/globe-2.jpg')",
+        mapBg: "url('../images/map_image.jpg')"
+        // mapBg: "url('./public/images/map_image.jpg')"
+      }
+    },
   },
   plugins: [],
 }
-
